d7998ee2b5b9ffc1f4dc9ed2bc8dd97d5b809450
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-04-29 22:16:53 +0100

    completed it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Header from "./components/Header";
+import Showcase from "./components/Showcase";
+import Boost from "./components/Boost";
+import Footer from "./components/Footer";
+import Advanced from "./components/Advanced";
+import Shortener from "./components/Shortener";
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header />
+      <Showcase />
+      <Shortener />
+      <Advanced />
+
+      <Boost />
+
+      <Footer />
+    </>
   );
 }
